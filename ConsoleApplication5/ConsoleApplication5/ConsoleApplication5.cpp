793bbc67977fe6c94e0d92b0ac00a7317a776de0
DeathByKorea <deathbykorea@gmail.com>
2017-06-18 00:25:09 +0100

    Yep

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,20 @@
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+#include <stdbool.h>
+using namespace std;
+
+int main()
+{
+	bool fazfriends = false;
+	if (fazfriends) {
+		cout << "Something has gone horribly wrong." << endl;
+		return 1;
+	}
+	else {
+		cout << "Faz has no friends" << endl;
+	}
+	return 0;
+}
